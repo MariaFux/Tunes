12043458fe0add19774259c17dd405e4e28e1a4b
мария пугачёва <mawytochka99@mail.ru>
2020-07-28 21:01:26 +0300

    Added hiding title from sections content

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,10 @@ import { musicPlayerInit } from './musicPlayer.js';
 
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
 
 const deactivationPlayer = () => {
+  temp.style.display = 'none';
   playerBtn.forEach((item) => { item.classList.remove('active')});
   playerBlock.forEach((item) => { item.classList.remove('active')});
 };

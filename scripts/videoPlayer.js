195294ba7e6b2ee9d04ca250c5fcc5d249dab226
мария пугачёва <mawytochka99@mail.ru>
2020-07-28 22:09:59 +0300

    Added play button toggle

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,13 +1,28 @@
 export const videoPlayerInit = () => {
 
   const videoPlayer = document.querySelector('.video-player');
+  const videoButtonPlay = document.querySelector('.video-button__play');
 
-  videoPlayer.addEventListener('click', () => {
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove('fa-pause');
+      videoButtonPlay.classList.add('fa-play');
+    } else {
+      videoButtonPlay.classList.add('fa-pause');
+      videoButtonPlay.classList.remove('fa-play');
+    }
+  }
+  
+  const togglePlay = () => {
     if (videoPlayer.paused) {
       videoPlayer.play();
     } else {
       videoPlayer.pause();
-    }    
-  });
+    }
+    
+    toggleIcon();
+  };
 
+  videoPlayer.addEventListener('click', togglePlay);
+  videoButtonPlay.addEventListener('click', togglePlay);
 };

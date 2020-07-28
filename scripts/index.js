797b19369d805df30538def7c0e2079b9efbbe5e
мария пугачёва <mawytochka99@mail.ru>
2020-07-28 20:57:31 +0300

    Added sections content activation

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 import { videoPlayerInit } from './videoPlayer.js';
 import { radioPlayerInit } from './radioPlayer.js';
 import { musicPlayerInit } from './musicPlayer.js'; 
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+
+//перебор кнопок разделов
+playerBtn.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    //активация стиля кнопки
+    btn.classList.add('active');
+    //активация содержимого раздела
+    playerBlock[i].classList.add('active');
+  })
+});

55aec5a8f626754c8ac4a7799c65916c8004eb65
мария пугачёва <mawytochka99@mail.ru>
2020-07-29 22:02:10 +0300

    Added play icon change

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -2,19 +2,40 @@ export const radioPlayerInit = () => {
 
   const radioNavigation = document.querySelector('.radio-navigation');
   const radioStop = document.querySelector('.radio-stop');
+  const radio = document.querySelector('.radio');
+  const radioItem = document.querySelectorAll('.radio-item');
 
   const audio = new Audio();
   audio.type = 'audio/aac';
 
   radioStop.disabled = true;
 
+  const changeIconPlay = () => {
+    if (audio.paused){
+      radio.classList.remove('play');
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop');
+    } else {
+      radio.classList.add('play');
+      radioStop.classList.remove('fa-play');
+      radioStop.classList.add('fa-stop');
+    }
+  };
+
+  const selectItem = elem => {
+    radioItem.forEach(item => item.classList.remove('select'));
+    elem.classList.add('select');
+  }
+
   radioNavigation.addEventListener('change', event => {
     const target = event.target;
+    const parrent = target.closest('.radio-item');
+    selectItem(parrent);
+
     radioStop.disabled = false;
-
     audio.src = target.dataset.radioStantion;
-
-    audio.play();    
+    audio.play();
+    changeIconPlay();    
   });
 
   radioStop.addEventListener('click', () => {
@@ -23,5 +44,6 @@ export const radioPlayerInit = () => {
     } else {
       audio.pause();
     }
+    changeIconPlay();
   });
 };

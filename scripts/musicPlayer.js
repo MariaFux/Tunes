4672280023b4d3cd30bfc6517e6fb36711cee538
мария пугачёва <mawytochka99@mail.ru>
2020-07-30 23:32:40 +0300

    Added playing music

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,1 +1,27 @@
-export const musicPlayerInit = () => {};
+export const musicPlayerInit = () => {
+
+  const audioNavigation = document.querySelector('.audio-navigation');
+  const audio = document.querySelector('.audio');
+  const audioButtonPlay = document.querySelector('.audio-button__play');
+  const audioPlayer = document.querySelector('.audio-player');
+
+  const playList = ['hello', 'flow', 'speed'];
+
+  let trackInndex = 0;
+  
+  audioNavigation.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.classList.contains('audio-button__play')) {
+      audio.classList.toggle('play');
+      audioButtonPlay.classList.toggle('fa-play');
+      audioButtonPlay.classList.toggle('fa-pause');
+
+      if (audioPlayer.paused){
+        audioPlayer.play();
+      } else {
+        audioPlayer.pause();
+      }
+    }
+  });
+};

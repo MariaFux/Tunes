1fff867635cb870119ca100ebb0806b5a9cbb7ca
мария пугачёва <mawytochka99@mail.ru>
2020-07-30 23:58:29 +0300

    Added prev and next buttons work

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -4,10 +4,27 @@ export const musicPlayerInit = () => {
   const audio = document.querySelector('.audio');
   const audioButtonPlay = document.querySelector('.audio-button__play');
   const audioPlayer = document.querySelector('.audio-player');
+  const audioImg = document.querySelector('.audio-img');
+  const audioHeader = document.querySelector('.audio-header');
 
   const playList = ['hello', 'flow', 'speed'];
 
-  let trackInndex = 0;
+  let trackIndex = 0;
+
+  const loadTrack = () => {
+    const isPlayed = audioPlayer.paused;
+    const track = playList[trackIndex];
+
+    audioImg.src = `./audio/${track}.jpg`;
+    audioHeader.textContent = track.toUpperCase();
+    audioPlayer.src = `./audio/${track}.mp3`;
+
+    if (isPlayed) {
+      audioPlayer.pause();
+    } else {
+      audioPlayer.play();
+    }
+  }
   
   audioNavigation.addEventListener('click', event => {
     const target = event.target;
@@ -22,6 +39,27 @@ export const musicPlayerInit = () => {
       } else {
         audioPlayer.pause();
       }
+
+      const track = playList[trackIndex];
+      audioHeader.textContent = track.toUpperCase();
+    }
+
+    if (target.classList.contains('audio-button__prev')) {
+      if (trackIndex !== 0) {
+        trackIndex--;
+      } else {
+        trackIndex = playList.length - 1;
+      }
+      loadTrack();
+    }
+
+    if (target.classList.contains('audio-button__next')) {
+      if (trackIndex === playList.length - 1) {
+        trackIndex = 0;
+      } else {
+        trackIndex++;
+      }
+      loadTrack();
     }
   });
 };
